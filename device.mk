1be3c70e5637eef864a7f6e618c2341085c12857
Deivid21 <david.parra.ignacio@gmail.com>
2025-09-06 01:37:59 -0400

    rhodep: Build MotCamera4 from prebuilt source
    Change-Id: I7a45d3b7db20dff97d64c5d473622bcea30cb44f
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -56,6 +56,12 @@ PRODUCT_PACKAGES += \
     init.nfc.sec.rc \
     init.oem.fingerprint2.sh
 
+# Moto Camera 4
+TARGET_MOTCAMERA4 := rhodep
+TARGET_USES_MOTCAMERA4 := true
+
+$(call inherit-product, vendor/motorola/MotCamera4-sm6375/motcamera4.mk)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.sec \
